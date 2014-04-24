c3eba708c5317d29dbb1a1c030ba632be005ec9f
Khriztian Moreno <khriztianmoreno@gmail.com>
2014-04-24 17:05:15 -0500

    Validación de campos del formulario
    Conociendo que no todos los navegadores soportan HTML5, inclui una
    validacion mediante JQUERY

--- WebUI/js/custom.js
+++ WebUI/js/custom.js
@@ -1,1 +1,14 @@
-custom.js
+function valida_envia(){ 
+   	//valido el nombre 
+   	var _name = document.getElementById("Fullname").value;
+   	if (_name.value.length==0){ 
+      	 alert("Tiene que escribir su nombre");
+      	 return 0; 
+   	} 
+
+   	
+
+   	//el formulario se envia 
+   	alert("Muchas gracias por enviar el formulario"); 
+   	document.contactform.submit(); 
+}
